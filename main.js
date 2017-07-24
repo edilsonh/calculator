42e2c1df1b920675818155d1e1eb68aba062338b
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-24 18:54:14 -0400

    refactored code to complete single arithmetic operation

--- main.js
+++ main.js
@@ -4,36 +4,41 @@ let clear = document.getElementById("clear");
 let equal = document.getElementById("equal");
 let decimal = document.getElementById("decimal");
 
-clear.addEventListener("click", remove);
-function remove() {
+clear.addEventListener("click", function remove() {
   display.innerHTML = "";
-}
+});
 
 for (let i = 0; i < num.length; i++) {
-    num[i].addEventListener("click", numbers);
-    function numbers() {
+    num[i].addEventListener("click", function numbers() {
       display.innerHTML += num[i].innerHTML;
-    }
+    });
 }
 
 for (let i = 0; i < op.length; i++) {
-  op[i].addEventListener("click", operate);
-  function operate() {
+  op[i].addEventListener("click", function operate() {
     if (op[i].innerHTML === "x") {
-      display.innerHTML += "*";
+      display.innerHTML += " * ";
     } else {
-    display.innerHTML += op[i].innerHTML;
+    display.innerHTML += ` ${op[i].innerHTML} `;
     }
-  }
+  });
 }
 
-equal.addEventListener("click", equate);
-function equate() {
-  let eq = eval(display.innerHTML);
-  display.innerHTML = eq;
-}
-
-decimal.addEventListener("click", deci);
-function deci() {
+decimal.addEventListener("click", function deci() {
   display.innerHTML += decimal.innerHTML;
-}
+});
+
+equal.addEventListener("click", function equate() {
+  let eq = display.innerHTML.split(" ");
+  let result = 0;
+  if (eq[1] === "/") {
+    result = parseInt(eq[0]) / parseInt(eq[2]);
+  } else if (eq[1] === "*") {
+    result = parseInt(eq[0]) * parseInt(eq[2]);
+  } else if (eq[1] === "-") {
+    result = parseInt(eq[0]) - parseInt(eq[2]);
+  } else if (eq[1] === "+") {
+    result = parseInt(eq[0]) + parseInt(eq[2]);
+  }
+  display.innerHTML = result;
+});
